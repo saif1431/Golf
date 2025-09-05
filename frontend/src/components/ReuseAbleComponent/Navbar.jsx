4d001fc2166c1263e06a84f64b2e6701fe5bd629
saif1431 <buttsaif539@gmail.com>
2025-09-05 23:11:17 +0500

    change About Us Link

--- frontend/src/components/ReuseAbleComponent/Navbar.jsx
+++ frontend/src/components/ReuseAbleComponent/Navbar.jsx
@@ -120,7 +120,7 @@ export default function Navbar() {
                 </Link>
                 <Link
                   className="text-gray-300 hover:text-white font-medium relative group transition-colors duration-200 border-b-2 border-black"
-                  href="/about-us"
+                  to="/about-us"
                 >
                   About
                   <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
